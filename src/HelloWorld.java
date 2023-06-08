d1ca0a4041b30ed8a5d2818244a2dc3f03a53e75
aVo7 <77satyr77@gmail.com>
2023-06-08 18:43:55 +0200

    inicializacia prveho projektu Git + oprava

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,6 +1,6 @@
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello world!");
-        System.out.println("Ahoj svet");
+        System.out.println("Ahoj svet a zajtra zas :D");
     }
 }
